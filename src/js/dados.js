ff8d350e30480241644e22fa2671fa6775ecbb6c
M4Y50N <pethersonreis@gmail.com>
2020-11-10 19:23:19 -0300

    Adicionei as FUNCIONALIDADES

---
+++ src/js/dados.js
@@ -0,0 +1,394 @@
+let Regist = [],
+	IsRegistradorContar = 0,
+	contadorDeEnderecosCompletos = 0,
+	intervaloDeChamadaDeFuncao,
+	ValorDeCadaTabela,
+	contadorDePassos = 0,
+	EnderoBusca = 1,
+	IsEnderecoContar = 0;
+
+function DeixarMaiusculo() {
+	document.getElementById("EntradaDeInstrucao").value = document
+		.getElementById("EntradaDeInstrucao")
+		.value.toUpperCase();
+}
+
+function AdicionarComando() {
+	var valorDeInput = document.getElementById("EntradaDeInstrucao").value;
+	var InstrucaoDecomposta = valorDeInput.split(" ");
+	var contadorDeCond = 0;
+	if (contadorDeEnderecosCompletos != 17) {
+		for (var contador = 0; contador < Instrucoes.length; contador++) {
+			contadorDeCond += 1;
+			if (3 <= InstrucaoDecomposta.length <= 4) {
+				if (InstrucaoDecomposta[0] == Instrucoes[contador]) {
+					if (
+						InstrucaoDecomposta.length == 4 &&
+						(InstrucaoDecomposta[0] == "ADD" ||
+							InstrucaoDecomposta[0] == "SUB" ||
+							InstrucaoDecomposta[0] == "DIV")
+					) {
+						Regist = [];
+						Regist.push(InstrucaoDecomposta[1]);
+						Regist.push(InstrucaoDecomposta[2]);
+						Regist.push(InstrucaoDecomposta[3]);
+
+						for (
+							var contador = 0;
+							contador < Registradores.length;
+							contador++
+						) {
+							for (
+								var contadorRegistrador = 0;
+								contadorRegistrador < Regist.length;
+								contadorRegistrador++
+							) {
+								if (Registradores[contador] == Regist[contadorRegistrador]) {
+									IsRegistradorContar += 1;
+								}
+							}
+						}
+						if (IsRegistradorContar == 3) {
+							var AlertErrorColor = document.getElementById(
+								"EntradaDeInstrucao"
+							);
+							AlertErrorColor.style = "border:2px solid green;";
+							InstrucoesDeEntrada.push(valorDeInput);
+							contadorDeEnderecosCompletos += 1;
+							AdicionarComandoNaMemoria(
+								contadorDeEnderecosCompletos,
+								valorDeInput
+							);
+							IsRegistradorContar = 0;
+							contadorDeCond = 0;
+							break;
+						} else {
+							var AlertErrorColor = document.getElementById(
+								"EntradaDeInstrucao"
+							);
+							AlertErrorColor.style = "border:2px solid red;";
+							alert(
+								"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
+							);
+							contadorDeCond = 0;
+							IsRegistradorContar = 0;
+							break;
+						}
+					} else {
+						if (
+							InstrucaoDecomposta.length == 3 &&
+							(InstrucaoDecomposta[0] == "LOAD" ||
+								InstrucaoDecomposta[0] == "STO")
+						) {
+							for (
+								var contador = 0;
+								contador < EnderecoInstrucaoDado.length;
+								contador++
+							) {
+								if (InstrucaoDecomposta[1] == EnderecoInstrucaoDado[contador]) {
+									IsRegistradorContar += 1;
+									Regist = [];
+									Regist.push(InstrucaoDecomposta[1]);
+									Regist.push(InstrucaoDecomposta[2]);
+
+									for (
+										var contador = 0;
+										contador < Registradores.length;
+										contador++
+									) {
+										if (Registradores[contador] == Regist[1]) {
+											IsRegistradorContar += 1;
+										}
+									}
+									if (IsRegistradorContar == 2) {
+										var AlertErrorColor = document.getElementById(
+											"EntradaDeInstrucao"
+										);
+										AlertErrorColor.style = "border:2px solid green;";
+										InstrucoesDeEntrada.push(valorDeInput);
+										contadorDeEnderecosCompletos += 1;
+										AdicionarComandoNaMemoria(
+											contadorDeEnderecosCompletos,
+											valorDeInput
+										);
+										IsRegistradorContar = 0;
+										contadorDeCond = 0;
+										IsEnderecoContar = 0;
+										break;
+									} else {
+										var AlertErrorColor = document.getElementById(
+											"EntradaDeInstrucao"
+										);
+										AlertErrorColor.style = "border:2px solid red;";
+										alert(
+											"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
+										);
+										contadorDeCond = 0;
+										IsRegistradorContar = 0;
+										IsEnderecoContar = 0;
+										break;
+									}
+								} else {
+									IsEnderecoContar += 1;
+								}
+							}
+							if (IsEnderecoContar == EnderecoInstrucaoDado.length) {
+								var AlertErrorColor = document.getElementById(
+									"EntradaDeInstrucao"
+								);
+								AlertErrorColor.style = "border:2px solid red;";
+								alert(
+									"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
+								);
+								contadorDeCond = 0;
+								IsRegistradorContar = 0;
+								IsEnderecoContar = 0;
+								break;
+							}
+						} else {
+							var AlertErrorColor = document.getElementById(
+								"EntradaDeInstrucao"
+							);
+							AlertErrorColor.style = "border:2px solid red;";
+							alert(
+								"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
+							);
+							contadorDeCond = 0;
+							IsRegistradorContar = 0;
+							IsEnderecoContar = 0;
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (contadorDeCond == Instrucoes.length) {
+			if (
+				parseInt(InstrucaoDecomposta[0]) == parseInt(InstrucaoDecomposta[0]) &&
+				parseInt(InstrucaoDecomposta[0]) != NaN
+			) {
+				//adicionar e criar tag de dado
+				var AlertErrorColor = document.getElementById("EntradaDeInstrucao");
+				AlertErrorColor.style = "border:2px solid green;";
+				DadosDeEntrada.push(valorDeInput);
+				contadorDeEnderecosCompletos += 1;
+				AdicionarComandoNaMemoria(contadorDeEnderecosCompletos, valorDeInput);
+				contadorDeCond = 0;
+				IsRegistradorContar = 0;
+				IsEnderecoContar = 0;
+			} else {
+				var AlertErrorColor = document.getElementById("EntradaDeInstrucao");
+				AlertErrorColor.style = "border:2px solid red;";
+				alert(
+					"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
+				);
+				contadorDeCond = 0;
+				IsRegistradorContar = 0;
+				IsEnderecoContar = 0;
+			}
+		}
+	} else {
+		var AlertErrorColor = document.getElementById("EntradaDeInstrucao");
+		AlertErrorColor.style = "border:2px solid red;";
+		alert("Quantidade de dados/Instrucões excedeu o limite!!");
+		contadorDeCond = 0;
+		IsRegistradorContar = 0;
+		IsEnderecoContar = 0;
+	}
+}
+
+// Contador de espaço na memoria
+function AdicionarComandoNaMemoria(id, Entrada) {
+	ValorDeCadaTabela = document.getElementById("td" + id).innerText;
+	if (ValorDeCadaTabela == "") {
+		document.getElementById("td" + id).innerText = Entrada;
+	}
+}
+
+//Vai executar os dados coletados anteriormente
+let EnderecoDaMemoriaTD,
+	EnderecoDaMemoriaTH,
+	InstrucaoRegister,
+	NextLine,
+	Endereco,
+	Reg1,
+	Reg2,
+	Reg3;
+
+//retagulos
+const PC_Rect = document.getElementById("PC"),
+	Reg1_Rect = document.getElementById("R1"),
+	Reg2_Rect = document.getElementById("R2"),
+	Reg3_Rect = document.getElementById("R3"),
+	Reg4_Rect = document.getElementById("R4"),
+	IR_Rect = document.getElementById("IR"),
+	UC_Rect = document.getElementById("UC"),
+	ULA_Rect = document.getElementById("ULA");
+//inputs
+let PC_Input = document.getElementById("input0"),
+	Reg1_Input = document.getElementById("input1"),
+	Reg2_Input = document.getElementById("input2"),
+	Reg3_Input = document.getElementById("input3"),
+	Reg4_Input = document.getElementById("input4"),
+	IR_Input = document.getElementById("input5"),
+	UC_Input = document.getElementById("input6"),
+	ULA_Input1 = document.getElementById("input7_1"),
+	ULA_Input2 = document.getElementById("input7_2");
+
+function AdicionarCalculosNaView(DadosGeral) {
+	VerificarSeEDadoOuInstrucao = +document.getElementById(
+		"td" + ContadorDeInstrucoes
+	).innerHTML;
+
+	EnderecoDaMemoriaTD = document.getElementById("td" + ContadorDeInstrucoes);
+	EnderecoDaMemoriaTH = document.getElementById("th" + ContadorDeInstrucoes);
+
+	let cont = 0,
+		intervalo,
+		ChangeColor = () => {
+			console.log(cont);
+			if (cont <= 2) {
+				if (cont == 0) {
+					PC_Input.innerHTML = ContadorDeInstrucoes;
+					MudarCorRect(PC_Rect);
+					ContadorDeInstrucoes++;
+				} else if (cont == 1) {
+					MudarCorRect(UC_Rect);
+				} else {
+					MudarCorTable(EnderecoDaMemoriaTD);
+					MudarCorTable(EnderecoDaMemoriaTH);
+					setTimeout(ChangeColorIfInstrucao, 1000);
+				}
+				cont == 2 ? clearInterval(intervalo) : cont++;
+			}
+		},
+		ChangeColorIfInstrucao = () => {
+			NextLine = DadosGeral[ContadorDeIndexDados];
+			InstrucaoRegister = NextLine.instrucao;
+			Endereco = NextLine.endereco;
+			Reg1 = NextLine.registrador1;
+			Reg2 = NextLine.registrador2;
+			Reg3 = NextLine.registrador_armazenamento;
+
+			ContadorDeIndexDados++;
+			//Faz o IR e UC piscarem
+			if (InstrucaoRegister != undefined) {
+				IR_Input.innerHTML = InstrucaoRegister;
+				MudarCorRect(IR_Rect);
+				UC_Input.innerHTML = InstrucaoRegister;
+				MudarCorRect(UC_Rect);
+				ResolveInstructions(InstrucaoRegister, Endereco, Reg1, Reg2, Reg3);
+			}
+		},
+		ResolveInstructions = (Instrucao, Endereco, Reg1, Reg2, Reg3) => {
+			setTimeout(function () {
+				PC_Input.innerHTML = ContadorDeInstrucoes;
+				MudarCorRect(PC_Rect);
+			}, 2000);
+			setTimeout(function () {
+				MudarCorRect(IR_Rect);
+			}, 3000);
+			if (Instrucao == "LOAD") {
+				setTimeout(function () {
+					MudarCorTable(document.getElementById("td" + Endereco));
+					MudarCorTable(document.getElementById("th" + Endereco));
+				}, 4000);
+
+				setTimeout(function () {
+					MudarCorRect(document.getElementById(Reg1));
+					document.getElementById("input" + Reg1[1]).innerHTML =
+						"0".repeat(
+							4 - document.getElementById("td" + Endereco).innerHTML.length
+						) + document.getElementById("td" + Endereco).innerHTML;
+				}, 5000);
+			} else if (Instrucao == "STO") {
+				setTimeout(function () {
+					MudarCorRect(document.getElementById(Reg1));
+				}, 4000);
+
+				setTimeout(function () {
+					MudarCorTable(document.getElementById("td" + Endereco));
+					MudarCorTable(document.getElementById("th" + Endereco));
+					document.getElementById(
+						"td" + Endereco
+					).innerHTML = document.getElementById("input" + Reg1[1]).innerHTML;
+				}, 5000);
+			} else if (Instrucao == "ADD") {
+				let Reg1_input = document.getElementById("input" + Reg1[1]),
+					Reg2_input = document.getElementById("input" + Reg2[1]),
+					Reg3_input = document.getElementById("input" + Reg3[1]);
+				Reg1 = document.getElementById(Reg1);
+				Reg2 = document.getElementById(Reg2);
+				Reg3 = document.getElementById(Reg3);
+				setTimeout(function () {
+					MudarCorRect(Reg1);
+					MudarCorRect(Reg2);
+				}, 4000);
+				setTimeout(function () {
+					MudarCorRect(ULA_Rect);
+					ULA_Input1.innerHTML = Reg1_input.innerHTML;
+					ULA_Input2.innerHTML = Reg2_input.innerHTML;
+				}, 5000);
+
+				setTimeout(function () {
+					MudarCorRect(Reg3);
+					let sum = +Reg1_input.innerHTML + +Reg2_input.innerHTML;
+					Reg3_input.innerHTML = "0".repeat(4 - sum.toString().length) + sum;
+				}, 6000);
+			} else if (Instrucao == "SUB") {
+				let Reg1_input = document.getElementById("input" + Reg1[1]),
+					Reg2_input = document.getElementById("input" + Reg2[1]),
+					Reg3_input = document.getElementById("input" + Reg3[1]);
+				Reg1 = document.getElementById(Reg1);
+				Reg2 = document.getElementById(Reg2);
+				Reg3 = document.getElementById(Reg3);
+				setTimeout(function () {
+					MudarCorRect(Reg1);
+					MudarCorRect(Reg2);
+				}, 4000);
+				setTimeout(function () {
+					MudarCorRect(ULA_Rect);
+					ULA_Input1.innerHTML = Reg1_input.innerHTML;
+					ULA_Input2.innerHTML = Reg2_input.innerHTML;
+				}, 5000);
+
+				setTimeout(function () {
+					MudarCorRect(Reg3);
+					let sub = +Reg1_input.innerHTML - +Reg2_input.innerHTML;
+					Reg3_input.innerHTML = "0".repeat(4 - sub.toString().length) + sub;
+				}, 6000);
+			} else if (Instrucao == "DIV") {
+				let Reg1_input = document.getElementById("input" + Reg1[1]),
+					Reg2_input = document.getElementById("input" + Reg2[1]),
+					Reg3_input = document.getElementById("input" + Reg3[1]);
+				Reg1 = document.getElementById(Reg1);
+				Reg2 = document.getElementById(Reg2);
+				Reg3 = document.getElementById(Reg3);
+				setTimeout(function () {
+					MudarCorRect(Reg1);
+					MudarCorRect(Reg2);
+				}, 4000);
+				setTimeout(function () {
+					MudarCorRect(ULA_Rect);
+					ULA_Input1.innerHTML = Reg1_input.innerHTML;
+					ULA_Input2.innerHTML = Reg2_input.innerHTML;
+				}, 5000);
+
+				setTimeout(function () {
+					MudarCorRect(Reg3);
+					let div =
+						+Reg2_input.innerHtml != 0
+							? +Reg1_input.innerHTML / +Reg2_input.innerHTML
+							: "Impossível Dividir";
+					Reg3_input.innerHTML = "0".repeat(4 - div.toString().length) + div;
+				}, 6000);
+			}
+		};
+
+	intervalo = setInterval(ChangeColor, 1000);
+}
+
+function IniciarPercusoDeInstrucoes() {
+	ColetarDadosDasFuncionalidades();
+	AdicionarCalculosNaView(DadosGeral);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,277 +1,55 @@
-var Regist = [];
-var IsRegistradorContar = 0;
-var contadorDeEnderecosCompletos = 0;
-var ValorDeCadaTabela;
-var contadorDePassos = 0;
-var EnderoBusca = 1;
-var IsEnderecoContar = 0;
-function IniciarPercusoDeInstrucoes() {
-	intervalosDeChamandaDeFuncao = setInterval(CoresDeCaixas, 500);
-}
+//Percorre o endereço de memoria
+let ContadorDeInstrucoes = 1,
+	ContadorDeIndexDados = 0;
 
-function CoresDeCaixas() {
-	var corDePC = document.getElementById("PC");
-	var corDaUC = document.getElementById("UC");
-	corDePC.style.fill = "green";
-	contadorDePassos += 1;
-	if (contadorDePassos > 3) {
-		corDePC.style.fill = "white";
-		if (contadorDePassos > 6 && contadorDePassos < 10) {
-			corDaUC.style.fill = "green";
-		} else {
-			corDaUC.style.fill = "white";
-			if (10 < contadorDePassos && contadorDePassos < 13) {
-				TrocarCorPorEndereco(EnderoBusca, "green");
-			} else {
-				if (contadorDePassos > 14) {
-					TrocarCorPorEndereco(EnderoBusca, "white");
-				}
-			}
+//Vai armazenar todos os valores para realizar as operações
+let DadosGeral = [];
+const ColetarDadosDasFuncionalidades = () => {
+	let ValorDoEndereco = "",
+		Instrucao = "";
+	for (let pos = 1; pos <= 17; pos++) {
+		(ValorDoEndereco = document
+			.getElementById("td" + pos)
+			.innerHTML.split(" ")),
+			(Instrucao = ValorDoEndereco[0]);
+		//LOAD ou STO
+		if (Instrucao == "LOAD" || Instrucao == "STO") {
+			let Endereco = ValorDoEndereco[1],
+				Registrador = ValorDoEndereco[2];
+			DadosGeral.push({
+				instrucao: Instrucao,
+				endereco: Endereco,
+				registrador1: Registrador,
+			});
+		} else if (Instrucao == "ADD" || Instrucao == "SUB" || Instrucao == "DIV") {
+			let Registrador1 = ValorDoEndereco[1],
+				Registrador2 = ValorDoEndereco[2],
+				RegistradorArmazenamento = ValorDoEndereco[3];
+
+			DadosGeral.push({
+				instrucao: Instrucao,
+				registrador1: Registrador1,
+				registrador2: Registrador2,
+				registrador_armazenamento: RegistradorArmazenamento,
+			});
+		} else if (!isNaN(parseInt(Instrucao)) || Instrucao == "") {
+			let Dado = Instrucao;
+			DadosGeral.push({ dado: Dado });
 		}
 	}
+};
+
+//muda a cor
+function MudarCorRect(elemento) {
+	elemento.classList.add("MudarCorRect");
+	setTimeout(() => {
+		elemento.classList.remove("MudarCorRect");
+	}, 750);
 }
 
-function AdicionarComando() {
-	var valorDeInput = document.getElementById("EntradaDeInstrucao").value;
-	var InstrucaoDecomposta = valorDeInput.split(" ");
-	var contadorDeCond = 0;
-	if (contadorDeEnderecosCompletos != 17) {
-		for (var contador = 0; contador < Instrucoes.length; contador++) {
-			contadorDeCond += 1;
-			if (3 <= InstrucaoDecomposta.length <= 4) {
-				if (InstrucaoDecomposta[0] == Instrucoes[contador]) {
-					if (
-						InstrucaoDecomposta.length == 4 &&
-						(InstrucaoDecomposta[0] == "ADD" ||
-							InstrucaoDecomposta[0] == "SUB" ||
-							InstrucaoDecomposta[0] == "DIV")
-					) {
-						Regist = [];
-						Regist.push(InstrucaoDecomposta[1]);
-						Regist.push(InstrucaoDecomposta[2]);
-						Regist.push(InstrucaoDecomposta[3]);
-
-						for (
-							var contador = 0;
-							contador < Registradores.length;
-							contador++
-						) {
-							for (
-								var contadorRegistrador = 0;
-								contadorRegistrador < Regist.length;
-								contadorRegistrador++
-							) {
-								if (Registradores[contador] == Regist[contadorRegistrador]) {
-									IsRegistradorContar += 1;
-								}
-							}
-						}
-						if (IsRegistradorContar == 3) {
-							var AlertErrorColor = document.getElementById(
-								"EntradaDeInstrucao"
-							);
-							AlertErrorColor.style = "border:2px solid green;";
-							InstrucoesDeEntrada.push(valorDeInput);
-							contadorDeEnderecosCompletos += 1;
-							AdicionarComandoNaMemoria(
-								contadorDeEnderecosCompletos,
-								valorDeInput
-							);
-							IsRegistradorContar = 0;
-							contadorDeCond = 0;
-							break;
-						} else {
-							var AlertErrorColor = document.getElementById(
-								"EntradaDeInstrucao"
-							);
-							AlertErrorColor.style = "border:2px solid red;";
-							alert(
-								"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
-							);
-							contadorDeCond = 0;
-							IsRegistradorContar = 0;
-							break;
-						}
-					} else {
-						if (
-							InstrucaoDecomposta.length == 3 &&
-							(InstrucaoDecomposta[0] == "LOAD" ||
-								InstrucaoDecomposta[0] == "STO")
-						) {
-							for (
-								var contador = 0;
-								contador < EnderecoInstrucaoDado.length;
-								contador++
-							) {
-								if (InstrucaoDecomposta[1] == EnderecoInstrucaoDado[contador]) {
-									IsRegistradorContar += 1;
-									Regist = [];
-									Regist.push(InstrucaoDecomposta[1]);
-									Regist.push(InstrucaoDecomposta[2]);
-
-									for (
-										var contador = 0;
-										contador < Registradores.length;
-										contador++
-									) {
-										if (Registradores[contador] == Regist[1]) {
-											IsRegistradorContar += 1;
-										}
-									}
-									if (IsRegistradorContar == 2) {
-										var AlertErrorColor = document.getElementById(
-											"EntradaDeInstrucao"
-										);
-										AlertErrorColor.style = "border:2px solid green;";
-										InstrucoesDeEntrada.push(valorDeInput);
-										contadorDeEnderecosCompletos += 1;
-										AdicionarComandoNaMemoria(
-											contadorDeEnderecosCompletos,
-											valorDeInput
-										);
-										IsRegistradorContar = 0;
-										contadorDeCond = 0;
-										IsEnderecoContar = 0;
-										break;
-									} else {
-										var AlertErrorColor = document.getElementById(
-											"EntradaDeInstrucao"
-										);
-										AlertErrorColor.style = "border:2px solid red;";
-										alert(
-											"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
-										);
-										contadorDeCond = 0;
-										IsRegistradorContar = 0;
-										IsEnderecoContar = 0;
-										break;
-									}
-								} else {
-									IsEnderecoContar += 1;
-								}
-							}
-							if (IsEnderecoContar == EnderecoInstrucaoDado.length) {
-								var AlertErrorColor = document.getElementById(
-									"EntradaDeInstrucao"
-								);
-								AlertErrorColor.style = "border:2px solid red;";
-								alert(
-									"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
-								);
-								contadorDeCond = 0;
-								IsRegistradorContar = 0;
-								IsEnderecoContar = 0;
-								break;
-							}
-						} else {
-							var AlertErrorColor = document.getElementById(
-								"EntradaDeInstrucao"
-							);
-							AlertErrorColor.style = "border:2px solid red;";
-							alert(
-								"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
-							);
-							contadorDeCond = 0;
-							IsRegistradorContar = 0;
-							IsEnderecoContar = 0;
-							break;
-						}
-					}
-				}
-			}
-		}
-		if (contadorDeCond == Instrucoes.length) {
-			if (
-				parseInt(InstrucaoDecomposta[0]) == parseInt(InstrucaoDecomposta[0]) &&
-				parseInt(InstrucaoDecomposta[0]) != NaN
-			) {
-				//adicionar e criar tag de dado
-				var AlertErrorColor = document.getElementById("EntradaDeInstrucao");
-				AlertErrorColor.style = "border:2px solid green;";
-				DadosDeEntrada.push(valorDeInput);
-				contadorDeEnderecosCompletos += 1;
-				AdicionarComandoNaMemoria(contadorDeEnderecosCompletos, valorDeInput);
-				contadorDeCond = 0;
-				IsRegistradorContar = 0;
-				IsEnderecoContar = 0;
-			} else {
-				var AlertErrorColor = document.getElementById("EntradaDeInstrucao");
-				AlertErrorColor.style = "border:2px solid red;";
-				alert(
-					"Ouve um erro na sua entrada, por favor revise o seu dado/instrução e adicione novamente!!"
-				);
-				contadorDeCond = 0;
-				IsRegistradorContar = 0;
-				IsEnderecoContar = 0;
-			}
-		}
-	} else  {
-		var AlertErrorColor = document.getElementById("EntradaDeInstrucao");
-		AlertErrorColor.style = "border:2px solid red;";
-		alert("Quantidade de dados/Instrucões excedeu o limite!!");
-		contadorDeCond = 0;
-		IsRegistradorContar = 0;
-		IsEnderecoContar = 0;
-	}
-}
-
-// function game() {
-// 	ContadorDeEspacoPercorrido += 100;
-// 	ObjectMov.setAttribute("cx", ContadorDeEspacoPercorrido);
-
-// 	if (ContadorDeEspacoPercorrido == 1000) {
-// 		clearInterval(intervalosDeChamandaDeFuncao);
-// 		console.log("fim de jogo!!");
-// 		ObjectMov.setAttribute("cx", 1200);
-// 		ContadorDeEspacoPercorrido = 0;
-// 	}
-// }
-
-// Contador de espaço na memoria
-function AdicionarComandoNaMemoria(id, Entrada) {
-	ValorDeCadaTabela = document.getElementById("td" + id).innerText;
-	if (ValorDeCadaTabela == "") {
-		document.getElementById("td" + id).innerText = Entrada;
-	}
-}
-
-function TrocarCorPorEndereco(id, cor) {
-	ValorDeCadaTabela = document.getElementById("tr" + id);
-	ValorDeCadaTabela.style.backgroundColor = cor;
-}
-
-function DeixarMaiusculo() {
-	document.getElementById("EntradaDeInstrucao").value = document.getElementById("EntradaDeInstrucao").value.toUpperCase()
-}
-
-// Variaveis de atribuição da instrução LOAD/STO
-let registrador = '', dado = '', intervalo = ''
-function Funcionalidades() {
-	let operacao = ''
-	for (let k = 1; k <= 17; k++) {
-		if (document.getElementById('td' + k).innerHTML != '') {
-			operacao = document.getElementById('td' + k).innerHTML
-			//LOAD
-			registrador = operacao.split(' ')[2], dado = operacao.split(' ')[1], operacao = operacao.split(' ')[0]
-			if (operacao == 'LOAD') {
-				//Adiciona no IR
-				document.getElementById('input5').innerHTML = operacao
-
-				document.getElementById('input6').innerHTML = operacao
-				for (let k = 1; k <= 4; k++) {
-					if (registrador == 'R' + k) {
-						document.getElementById('input' + k).innerHTML = dado
-					}
-				}
-			}
-			//STO
-			if (operacao == 'STO') {
-				//Adiciona no IR
-				document.getElementById('input5').innerHTML = operacao
-
-				document.getElementById('input6').innerHTML = operacao
-				document.getElementById('td' + dado).innerHTML = document.getElementById('input' + registrador[1]).innerHTML
-			}
-		}
-	}
+function MudarCorTable(elemento) {
+	elemento.classList.add("MudarCorTable");
+	setTimeout(() => {
+		elemento.classList.remove("MudarCorTable");
+	}, 750);
 }
